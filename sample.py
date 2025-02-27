28599fe4a3404fdf672824c62b3eb9b9a3b260d3
gibsoche <123422385+bing-codes@users.noreply.github.com>
2025-02-27 09:57:45 -0500

    updated assignment

---
+++ sample.py
@@ -0,0 +1,36 @@
+#this is a sample .py file that we can try branching with 
+#to keep things less chaotic, I'm giving you each a little bit of code to write
+
+#Beatrice, write a for loop under this comment
+
+#Charlie, write a while loop under this comment
+
+#Matthew (D'Angelo), write a list under this comment
+
+#Nicholas (DeMatteo), import a library under this comment
+
+#Talia, create a simple function under this comment
+
+#Jonathan, create a dictionary under this comment
+
+#Corey, print of something silly under this comment
+
+#Nicholas (Guarino), create a short conditonal block under this comment
+
+#Aidan, randomly choose a number using random under this comment
+
+#Mina, import a .txt file of your choosing under this comment, make sure the file is in the repo too
+
+#Christian, create a ranged for loop under this comment
+
+#Fayaz, create a simple program under this comment that incorporates your name
+
+#Ricky, throw in one of our API calls under this comment
+
+#Sharon, create a list of dictionaries under this comment
+
+#Toyice, use a string method under this comment
+
+#Will, use a list method under this comment
+
+#Jake, index a list under this comment
